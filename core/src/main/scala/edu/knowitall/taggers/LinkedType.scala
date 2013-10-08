6a34b9ee05553b6d0da60f3040c413b3c6f30187
John Gilmer <jgilme1@porvo.cs.washington.edu>
2013-10-08 16:33:46 -0700

    made toString method emulate case class implementation

--- core/src/main/scala/edu/knowitall/taggers/LinkedType.scala
+++ core/src/main/scala/edu/knowitall/taggers/LinkedType.scala
@@ -2,7 +2,6 @@ package edu.knowitall.taggers
 
 import edu.knowitall.tool.typer.Type
 import edu.knowitall.common.HashCodeHelper
-import edu.knowitall.collection.immutable.Interval
 
 class LinkedType(val typ: Type, val link: Option[Type] = None) extends Type {
   override def name = typ.name
@@ -10,7 +9,7 @@ class LinkedType(val typ: Type, val link: Option[Type] = None) extends Type {
   override def tokenInterval = typ.tokenInterval
   override def text = typ.text
   
-  override def toString() = "(" + name +","+source+","+tokenInterval+","+text+")"
+  override def toString() = "LinkedType(" + name +","+source+","+tokenInterval+","+text+")"
 
   override def equals(that:Any) = that match{
     case that: LinkedType =>  that.canEqual(this) && that.hashCode() == this.hashCode()
@@ -21,7 +20,7 @@ class LinkedType(val typ: Type, val link: Option[Type] = None) extends Type {
   
 }
 class NamedGroupType(val groupName: String, typ: Type, link: Option[Type] = None) extends LinkedType(typ,link){
-  override def toString() = "(" + groupName + "," +  name +","+source+","+tokenInterval+","+text+")"
+  override def toString() = "NamedGroupType(" + groupName + "," +  name +","+source+","+tokenInterval+","+text+")"
   override def canEqual(that: Any) = that.isInstanceOf[NamedGroupType]
   override def hashCode = HashCodeHelper(groupName,this.name,this.tokenInterval,this.text,this.link)
   override def equals(that:Any) = that match{
