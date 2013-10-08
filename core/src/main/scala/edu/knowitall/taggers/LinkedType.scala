052a0f43fc1cf1c7e259d05af48a087a93095b32
John Gilmer <jgilme1@porvo.cs.washington.edu>
2013-10-08 11:12:02 -0700

    Added a NamedGroupType to separate them from unnamed groups

--- core/src/main/scala/edu/knowitall/taggers/LinkedType.scala
+++ core/src/main/scala/edu/knowitall/taggers/LinkedType.scala
@@ -2,9 +2,11 @@ package edu.knowitall.taggers
 
 import edu.knowitall.tool.typer.Type
 
-case class LinkedType(typ: Type, link: Option[Type] = None) extends Type {
+class LinkedType(val typ: Type, val link: Option[Type] = None) extends Type {
   override def name = typ.name
   override def source = typ.source
   override def tokenInterval = typ.tokenInterval
   override def text = typ.text
+}
+class NamedGroupType(val groupName: String, typ: Type, link: Option[Type] = None) extends LinkedType(typ,link){
 }
