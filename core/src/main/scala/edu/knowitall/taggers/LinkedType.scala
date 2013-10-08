1b95131a27a232f895cb08351398f63dc164e697
John Gilmer <jgilme1@porvo.cs.washington.edu>
2013-10-08 13:39:48 -0700

    Added equality and hasCode methods for NamedGroupType and LinkedType

--- core/src/main/scala/edu/knowitall/taggers/LinkedType.scala
+++ core/src/main/scala/edu/knowitall/taggers/LinkedType.scala
@@ -1,12 +1,32 @@
 package edu.knowitall.taggers
 
 import edu.knowitall.tool.typer.Type
+import edu.knowitall.common.HashCodeHelper
+import edu.knowitall.collection.immutable.Interval
 
 class LinkedType(val typ: Type, val link: Option[Type] = None) extends Type {
   override def name = typ.name
   override def source = typ.source
   override def tokenInterval = typ.tokenInterval
   override def text = typ.text
+  
+  override def toString() = "(" + name +","+source+","+tokenInterval+","+text+")"
+
+  override def equals(that:Any) = that match{
+    case that: LinkedType =>  that.canEqual(this) && that.hashCode() == this.hashCode()
+    case _ => false
+  }
+  def canEqual(that: Any) = that.isInstanceOf[LinkedType]
+  override def hashCode = HashCodeHelper(name,tokenInterval,text,link)
+  
 }
 class NamedGroupType(val groupName: String, typ: Type, link: Option[Type] = None) extends LinkedType(typ,link){
+  override def toString() = "(" + groupName + "," +  name +","+source+","+tokenInterval+","+text+")"
+  override def canEqual(that: Any) = that.isInstanceOf[NamedGroupType]
+  //override def hashCode = HashCodeHelper(groupName,this.name,this.source,this.tokenInterval,this.text,this.link)
+  override def hashCode = HashCodeHelper(groupName,this.name,this.tokenInterval,this.text,this.link)
+  override def equals(that:Any) = that match{
+    case that: NamedGroupType => that.canEqual(this) && that.hashCode() == this.hashCode()
+    case _ => false
+  }
 }
