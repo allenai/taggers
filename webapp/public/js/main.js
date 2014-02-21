726829300504a90bf46ce2956956b880503f2f96
Michael Schmitz <michael@schmitztech.com>
2014-02-21 09:12:44 -0800

    Move example into js.

--- webapp/public/js/main.js
+++ webapp/public/js/main.js
@@ -1,8 +1,17 @@
 angular.module('taggers', ['ui.bootstrap']);
 var TaggersCtrl = function($scope, $http) {
+  $scope.taggersModel = { }
+
   $http.get('/fields').then(function(response) {
     $scope.taggersModel = response.data;
   });
+
+  $scope.showExample = function() {
+    $scope.taggersModel.sentences = "The fat black cat was hidden in the dark corner."; 
+    $scope.taggersModel.extractors = ""; 
+    $scope.taggersModel.taggers = "AnimalTagger := LemmatizedKeywordTagger( cat )";
+  }
+
   $scope.submit = function() {
     $http.post("/", $scope.taggersModel).then(
       function(response) {
