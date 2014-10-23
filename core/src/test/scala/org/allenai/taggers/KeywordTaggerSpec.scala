9f1a2c8119e8b2dc3807e08b8f32d258ba0eca82
Michael Schmitz <MichaelS@allenai.org>
2014-10-22 18:00:02 -0700

    Add tests

--- core/src/test/scala/org/allenai/taggers/KeywordTaggerSpec.scala
+++ core/src/test/scala/org/allenai/taggers/KeywordTaggerSpec.scala
@@ -13,20 +13,42 @@ class KeywordTaggerSpec extends FlatSpec {
   val chunker = new OpenNlpChunker();
   val morpha = new MorphaStemmer();
 
-  val runTagger = new KeywordTagger("RoadTagger", Seq("road"))
-
-  "a keyword tagger" should "match the last token in a sentence" in {
-    val sentenceText = "The man had run down the road"
-    val opennlpChunker = new OpenNlpChunker
-    val s = new Sentence(sentenceText) with Chunker with Lemmatizer with Consume {
+  def parse(s: String) = {
+    new Sentence(s) with Chunker with Lemmatizer with Consume {
       override val chunker = new OpenNlpChunker
       override val postagger = defaultPostagger
       override val tokenizer = defaultTokenizer
       override val lemmatizer = MorphaStemmer
     }
+  }
 
-    val types = runTagger.apply(s)
+  "a keyword tagger" should "match the last token in a sentence" in {
+    val runTagger = new KeywordTagger("RoadTagger", Seq("road"))
+    val sentenceText = "The man had run down the road"
+    val opennlpChunker = new OpenNlpChunker
+
+    val types = runTagger.apply(parse(sentenceText))
 
     assert(types.size === 1)
+  }
+
+  "a multi-word keyword tagger" should "match correctly" in {
+    val tagger = new KeywordTagger("RoadTagger", Seq("rail road"))
+    val sentenceText = "The man had run down the rail road"
+    val opennlpChunker = new OpenNlpChunker
+
+    val types = tagger.apply(parse(sentenceText))
+
+    assert(types.size === 1)
+  }
+
+  "a multi-word keyword tagger" should "match twice" in {
+    val tagger = new KeywordTagger("RoadTagger", Seq("rail road"))
+    val sentenceText = "rail road oh my rail road"
+    val opennlpChunker = new OpenNlpChunker
+
+    val types = tagger.apply(parse(sentenceText))
+
+    assert(types.size === 2)
   }
 }
