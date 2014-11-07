88361031965cc9bd37a0f7c2c4015dcf0de26680
Michael Schmitz <michael@schmitztech.com>
2014-11-06 17:18:47 -0800

    Use all public dependencies.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,14 +11,14 @@ object Dependencies {
       "com.github.scopt" %% "scopt" % "3.2.0")
 
   val slf4j = "org.slf4j" % "slf4j-api" % "1.7.6"
-  val logbackVersion = "1.1.1"
+  val logbackVersion = "1.1.2"
   val logbackCore = "ch.qos.logback" % "logback-core" % logbackVersion
   val logbackClassic = "ch.qos.logback" % "logback-classic" % logbackVersion
   val loggingImplementations = Seq(logbackCore, logbackClassic)
 
   val sprayJson = "io.spray" %% "spray-json" % "1.2.6"
 
-  def nlpstackModule(id: String) = "org.allenai.nlpstack" %% s"nlpstack-$id" % "0.13"
+  def nlpstackModule(id: String) = "org.allenai.nlpstack" %% s"nlpstack-$id" % "0.16"
   def akkaModule(id: String) = "com.typesafe.akka" %% s"akka-$id" % "2.3.2"
   def sprayModule(id: String) = "io.spray" %% s"spray-$id" % "1.3.1"
 }
